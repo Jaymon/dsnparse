24989291e4ac2d15200fcf82d14a2aa7f32cceea
Jay Marcyes <jay@marcyes.com>
2013-09-16 15:14:39 -0700

    made the results object a little easier to use based on how I am using it in other libraries

--- dsnparse.py
+++ dsnparse.py
@@ -2,28 +2,30 @@ import urlparse
 import re
 import os
 
-__version__ = '0.1'
+__version__ = '0.1.2'
 
-def parse_environ(name):
+def parse_environ(name, **default_vals):
     """
     same as parse() but you pass in an environment variable name that will be used
     to fetch the dsn
 
     name -- string -- the environment variable name that contains the dsn to parse
+    **default_vals -- dict -- any values you want to have defaults for if they aren't in the dsn
     return -- ParseResult() tuple
     """
-    return parse(os.environ[name])
+    return parse(os.environ[name], **default_vals)
 
-def parse(dsn):
+def parse(dsn, **default_vals):
     """
     parse a dsn to parts similar to parseurl
 
     this is a nuts function that can serve as a good basis to parsing a custom dsn
 
     dsn -- string -- the dsn to parse
+    **default_vals -- dict -- any values you want to have defaults for if they aren't in the dsn
     return -- ParseResult() tuple
     """
-    assert re.match("^\S+://\S+", dsn), "only full dsns with form: scheme://host... are supported"
+    assert re.match("^\S+://\S+", dsn), "{} is invalid, only full dsn urls (scheme://host...) allowed".format(dsn)
 
     first_colon = dsn.find(':')
     scheme = dsn[0:first_colon]
@@ -40,21 +42,23 @@ def parse(dsn):
                 options[k] = kv[0]
 
     r = ParseResult(
-        scheme,
-        url.netloc,
-        url.path,
-        url.params,
-        options,
-        url.fragment,
+        scheme=scheme,
+        hostname=url.hostname,
+        path=url.path,
+        params=url.params,
+        query=options,
+        fragment=url.fragment,
         username=url.username,
         password=url.password,
-        hostname=url.hostname,
         port=url.port,
         query_str=url.query,
     )
+    for k, v in default_vals.iteritems():
+        r.setdefault(k, v)
+
     return r
 
-class ParseResult(urlparse.ParseResult):
+class ParseResult(object):
     """
     hold the results of a parsed dsn
 
@@ -66,7 +70,7 @@ class ParseResult(urlparse.ParseResult):
         scheme
         schemes -- if your scheme has +'s in it, then this will contain a list of schemes split by +
         path
-        paths -- the path segment split by /
+        paths -- the path segment split by /, so "/foo/bar" would be ["foo", "bar"]
         host -- same as hostname (I just like host better)
         hostname
         hostloc -- host:port
@@ -77,21 +81,44 @@ class ParseResult(urlparse.ParseResult):
         query_str -- the raw query string
         port
         fragment
+        anchor -- same as fragment, just an alternative name
     """
-    @classmethod
-    def __new__(cls, *args, **kwargs):
-        # http://stackoverflow.com/questions/10788976/how-do-i-properly-inherit-from-a-superclass-that-has-a-new-method
-        return urlparse.ParseResult.__new__(*args)
-
-    def __init__(self, *args, **kwargs):
-        super(ParseResult, self).__init__(*args)
+    def __init__(self, **kwargs):
         for k, v in kwargs.iteritems():
-            self.__dict__[k] = v
+            setattr(self, k, v)
+
+    def __iter__(self):
+        mapping = ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']
+        for k in mapping:
+            yield getattr(self, k, '')
+
+    def __getitem__(self, index):
+        index = int(index)
+        mapping = {
+            0: 'scheme',
+            1: 'netloc',
+            2: 'path',
+            3: 'params',
+            4: 'query',
+            5: 'fragment',
+        }
+
+        return getattr(self, mapping[index], '')
 
     @property
     def schemes(self):
         """the scheme, split by plus signs"""
         return self.scheme.split('+')
+
+    @property
+    def netloc(self):
+        """return username:password@hostname:port"""
+        s = ''
+        if self.username and self.password:
+            s += "{}:{}@".format(self.username, self.password)
+
+        s += self.hostloc
+        return s
 
     @property
     def paths(self):
@@ -107,10 +134,29 @@ class ParseResult(urlparse.ParseResult):
     def hostloc(self):
         """return host:port"""
         hostloc = self.hostname
-        if self.port is not None:
+        if self.port:
             hostloc = '{}:{}'.format(hostloc, self.port)
 
         return hostloc
+
+    @property
+    def anchor(self):
+        """alternative name for the fragment"""
+        return self.fragment
+
+    def setdefault(self, key, val):
+        """
+        set a default value for key
+
+        this is different than dict's setdefault because it will set default either
+        if the key doesn't exist, or if the value at the key evaluates to False, so
+        an empty string or a None will value will be updated
+
+        key -- string -- the attribute to update
+        val -- mixed -- the attributes new value if key has a current value that evaluates to False
+        """
+        if not getattr(self, key, None):
+            setattr(self, key, val)
 
     def geturl(self):
         """return the dsn back into url form"""
--- dsnparse_test.py
+++ dsnparse_test.py
@@ -71,8 +71,46 @@ class DsnParseTest(TestCase):
 
     def test_parse_environ(self):
         os.environ['TEST_DSN'] = 'scheme://username:password@host:1234/foo'
-        r = dsnparse.parse('TEST_DSN')
+        r = dsnparse.parse_environ('TEST_DSN')
         self.assertEqual(os.environ['TEST_DSN'], r.geturl())
 
+    def test_unpack(self):
+        dsn = 'scheme://username:password@host:1234/foo'
+        dsn_test = {
+            'scheme': 'scheme',
+            'netloc': 'username:password@host:1234',
+            'path': '/foo',
+            'params': "",
+            'query': {},
+            'fragment': ''
+        }
+        scheme, netloc, path, params, query, fragment = dsnparse.parse(dsn)
+        self.assertEqual('scheme', scheme)
+        self.assertEqual('username:password@host:1234', netloc)
+        self.assertEqual('/foo', path)
+        self.assertEqual('', params)
+        self.assertEqual({}, query)
+        self.assertEqual('', fragment)
+
+    def test___getitem__(self):
+        dsn = 'scheme://username:password@host:1234/foo'
+        r = dsnparse.parse(dsn)
+        self.assertEqual('scheme', r[0])
+        self.assertEqual('username:password@host:1234', r[1])
+        self.assertEqual('/foo', r[2])
+        self.assertEqual('', r[3])
+        self.assertEqual({}, r[4])
+        self.assertEqual('', r[5])
+
+    def test_setdefault(self):
+        dsn = 'scheme://username:password@host/foo'
+        r = dsnparse.parse(dsn)
+        self.assertEqual(None, r.port)
+
+        r.setdefault('port', 1234)
+        self.assertEqual(1234, r.port)
+
+        r = dsnparse.parse(dsn, port=1235)
+        self.assertEqual(1235, r.port)
 
 
